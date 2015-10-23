0cdc8f7f29a2d2bd88b1dd240f2551468187455f
nalinibansal <bansal.nalini23@gmail.com>
2015-10-23 18:30:59 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A pair of funcitons that cache the inverse of a matrix
 
-## Write a short comment describing this function
+## Creates a matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+         }
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
+## If the inverse has already been calculated (and the matrix has not
+## changed), then the "cachesolve" should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data.")
+        return(inv)
+        }
+    data <- x$get()
+    inv <- solve(data)
+    x$setinverse(inv)
+    inv
 }
+
+x = rbind(c(1,2),c(1,3))
+> m=makeCacheMatrix(x)
+> m$get()
+     [,1] [,2]
+[1,]    1    2
+[2,]    1    3
+>  cacheSolve(m)
+     [,1] [,2]
+[1,]    3   -2
+[2,]   -1    1
+> cacheSolve(m)
+getting cached data.
+     [,1] [,2]
+[1,]    3   -2
+[2,]   -1    1
+> 
